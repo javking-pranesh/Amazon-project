a9444dfb6e4b620b1bcf15b2a1cbe5fae61a9cd7
javking-pranesh <javkingpranesh18@gmail.com>
2025-07-29 22:14:13 +0530

    Delete Products from html when delete is clicked

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -15,7 +15,7 @@ cart.forEach((cartItem)=>{
     
 
     cartSummaryHTML=cartSummaryHTML + `
-     <div class="cart-item-container">
+     <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -101,6 +101,11 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
         const productId=link.dataset.productId;
         removeFromCart(productId);
         console.log(cart)
+
+       var container= document.querySelector(`.js-cart-item-container-${productId}`);
+
+       container.remove();
     });
+    
     
 });
